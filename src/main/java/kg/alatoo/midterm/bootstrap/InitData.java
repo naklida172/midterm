ad0d76f4e573a69ded008c545c2d7c7c429e5ed7
naklida172 <homewer2016@gmail.com>
2025-03-16 01:07:55 +0600

    start of proper work in regards to bootstrap

--- src/main/java/kg/alatoo/midterm/bootstrap/InitData.java
+++ src/main/java/kg/alatoo/midterm/bootstrap/InitData.java
@@ -1,58 +1,55 @@
-// package kg.alatoo.midterm.bootstrap;
+package kg.alatoo.midterm.bootstrap;
 
-// import jakarta.annotation.PostConstruct;
-// import jakarta.annotation.PreDestroy;
-// import kg.alatoo.midterm.entities.*;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import kg.alatoo.midterm.entities.*;
+import kg.alatoo.midterm.repositories.OrderRepository;
+import kg.alatoo.midterm.repositories.PointRepository;
+import kg.alatoo.midterm.repositories.ProductRepository;
+import kg.alatoo.midterm.repositories.SellerRepository;
+import kg.alatoo.midterm.repositories.TagRepository;
+import kg.alatoo.midterm.repositories.UserRepository;
 
-// import java.util.Date;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-// @Component
-// public class InitData {
+@Component
+public class InitData {
 
-//     @Autowired
-//     private OrderRepository orderRepository;
-//     @Autowired
-//     private ProductRepository productRepository;
-//     @Autowired
-//     private PointRepository pointRepository;
-//     @Autowired
-//     private UserRepository userRepository;
-//     @Autowired
-//     private SellerRepository sellerRepository;
-//     @Autowired
-//     private TagRepository tagRepository;
+    @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private PointRepository pointRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private SellerRepository sellerRepository;
+    @Autowired
+    private TagRepository tagRepository;
 
-//     @PostConstruct
-//     public void init() {
+    private Map<Long, Seller> sellers = new HashMap<>();
+    private Map<Long, User> users = new HashMap<>();
+    private Map<Long, Product> products = new HashMap<>();
+    private Map<Long, Point> points = new HashMap<>();
+    private Map<Long, Tag> tags = new HashMap<>();
 
-//         System.out.println("Initializing data");
+    private long currentId = 1;
 
-//         Seller seller = new Seller(0, "SellerName", null);
-//         sellerRepository.save(seller);
+    @PostConstruct
+    public void init() {
+        System.out.println("Creating values from src\\main\\java\\kg\\alatoo\\midterm\\bootstrap\\InitData.csv");
+        
 
-//         Product product = new Product(0, "ProductName", seller, "Description", (short) 5, null);
-//         productRepository.save(product);
+    }
 
-//         User user = new User(0, "UserName", "Username", "Role", "Email", "Phone", null);
-//         userRepository.save(user);
-
-//         Point point = new Point(0, "Address", "Status", "WorkTime", null);
-//         pointRepository.save(point);
-
-//         Order order = new Order(0, new Date(), (short) 10, "Status", product, point, user);
-//         orderRepository.save(order);
-
-//         Tag tag = new Tag(0, "TagName", "Description", null);
-//         tagRepository.save(tag);
-
-//         System.out.println("Data initialization completed.");
-//     }
-
-//     @PreDestroy
-//     public void destroy() {
-//         System.out.println("Cleaning up resources");
-//     }
-// }
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Cleaning up resources");
+    }
+}
