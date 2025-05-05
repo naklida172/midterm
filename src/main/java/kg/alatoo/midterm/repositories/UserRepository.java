775db15a2b98d09402632ff6a0284200344d0788
naklida172 <homewer2016@gmail.com>
2025-05-05 23:29:18 +0600

    Secrets fixed

--- src/main/java/kg/alatoo/midterm/repositories/UserRepository.java
+++ src/main/java/kg/alatoo/midterm/repositories/UserRepository.java
@@ -10,6 +10,7 @@ import kg.alatoo.midterm.entities.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String username);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
 }
